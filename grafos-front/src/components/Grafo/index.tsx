5df89c49bc3c98571548dc9ac7098f69b69d1ad2
josedhonatas <dhonatas@academico.ufs.br>
2023-06-08 17:13:52 -0300

    teste_grafo

--- grafos-front/src/components/Grafo/index.tsx
+++ grafos-front/src/components/Grafo/index.tsx
@@ -1,63 +1,73 @@
-import React, { useEffect, useRef } from 'react';
-import { Network } from 'vis-network';
-import { GrafoLib } from '../../models/GrafoLib';
+import React, { useRef, useEffect } from 'react';
+import { Network, Node, Edge } from 'vis-network';
 
 const Grafo: React.FC = () => {
     const containerRef = useRef<HTMLDivElement | null>(null);
     let network: Network | null = null;
+    let nodes: Node[] = [
+        { id: 1, label: '1', x: 100, y: 100 },
+        { id: 2, label: '2', x: 200, y: 200 },
+        { id: 3, label: '3', x: 300, y: 300 },
+    ];
+    let edges: Edge[] = [
+        { from: 1, to: 2 },
+        { from: 1, to: 3 },
+    ];
 
-    const grafo = new GrafoLib();
-    grafo.adicionarVertice(1);
-    grafo.adicionarVertice(2);
-    grafo.adicionarVertice(3);
-    grafo.adicionarVertice(4);
-    grafo.exibirGrafo();
+    const handleContainerClick = (event: MouseEvent) => {
+        if (network) {
+            const position = network.canvasToDOM({ x: event.clientX, y: event.clientY });
+            const id = nodes.length + 1;
+            const newNode: Node = {
+                id,
+                label: id.toString(),
+                x: position.x,
+                y: position.y,
+            };
+            nodes = [...nodes, newNode];
+            network.setData({ nodes, edges });
+        }
+    };
 
     useEffect(() => {
         if (containerRef.current) {
-            // Dados dos nós
-            const nodes = [
-                { id: 1, label: '1' },
-                { id: 2, label: '2' },
-                { id: 3, label: '3' },
-            ];
-
-            // Dados das arestas
-            const edges = [
-                { from: 1, to: 2 },
-                { from: 1, to: 3 },
-            ];
-
             // Opções de configuração do grafo
             const options = {
                 nodes: {
-                    shape: 'circle', // Forma do texto do vertice
+                    shape: 'circle',
                     font: {
-                        size: 20, // Tamanho do texto do vertice
+                        size: 20,
                     },
-                    size: 20, // tamanho vertices
-                    borderWidth: 2, // Ajuste a largura da borda
-                    //borderRadius: 2, // Ajuste o raio da borda para torná-la mais arredondada
+                    size: 20,
+                    borderWidth: 2,
                 },
-
                 edges: {
                     width: 3,
                     color: {
-                        color: 'gray', // Cor padrão das arestas
-                        highlight: 'black', // Cor quando a aresta é destacada
+                        color: 'gray',
+                        highlight: 'black',
                     },
-                }
+                },
+                interaction: {
+                    dragNodes: true,
+                },
             };
 
             // Criar a instância do grafo
             network = new Network(containerRef.current, { nodes, edges }, options);
+
+            // Adicionar o evento de clique ao contêiner do grafo
+            containerRef.current.addEventListener('click', handleContainerClick);
         }
 
-        // Limpar a instância do grafo ao desmontar o componente
+        // Limpar a instância do grafo e remover o evento ao desmontar o componente
         return () => {
             if (network) {
                 network.destroy();
                 network = null;
+            }
+            if (containerRef.current) {
+                containerRef.current.removeEventListener('click', handleContainerClick);
             }
         };
     }, []);
